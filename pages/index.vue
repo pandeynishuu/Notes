4b59776674b2c29dc4a072df9c17fa77655bb015
Nisha Pandey <np0091459@gmail.comm>
2023-05-10 21:10:22 +0545

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+       <img src="https://cdn.pixabay.com/photo/2017/12/09/21/33/sunset-3008779_960_720.jpg" alt="" style="objectFit:cover; width:100%; height:100
+       ]%;">
+      
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>    
